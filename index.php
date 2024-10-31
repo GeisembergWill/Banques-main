5a6d135de0daa0c8941b3e7b0858bba2516c80cc
GeisembergWill <wgm@live.fr>
2024-10-31 23:12:06 +0100

    php banques

--- index.php
+++ index.php
@@ -15,47 +15,47 @@ $compte2 = new Compte("Livret Jeune", 500, "€", $titulaire);
 $compte3 = new Compte("Livret PEL", 500, "€", $titulaire);
 $compte4 = new Compte("Livret Bloqué", 500, "€", $titulaire);
 
-// Affiche le titulaire et son âge
-echo "Les Comptes bancaires de Mr $titulaire, " . $titulaire->getAge() . " ans au Crédit Dauphin.<br>";
 
+// Affiche le titulaire et son âge
+echo "Les Comptes bancaires de Mr $titulaire (68100/Haut-Rhin), " . $titulaire->getAge() . " ans au Crédit Dauphin.<br>";
+echo "Soldes initiale des comptes.<br>";//solde intiales
 // Fonction pour afficher le solde d'un compte
 function afficherSolde($compte) {
     // Affiche le libellé du compte, le solde actuel et la devise
     echo "Solde de {$compte->getLibelee()}: {$compte->getSoldeInitial()} {$compte->getDevise()}<br>";
 }
-
+// Affiche les informations de tous les comptes
+echo "$compte1 $compte2 $compte3 $compte4<br><br>";
+echo "Mouvements Bancaires. Crediter de 100 sur tous les comptes <br>";
 // Crédite 100 à compte 1 a 4 et affiche le nouveau solde
 $compte1->crediter(100); 
-afficherSolde($compte1); 
+afficherSolde($compte1)."<br>";  
 
 $compte2->crediter(100);
-afficherSolde($compte2); 
+afficherSolde($compte2)."<br>"; 
 
 $compte3->crediter(100); 
-afficherSolde($compte3); 
+afficherSolde($compte3)."<br>";  
 
 $compte4->crediter(100); 
-afficherSolde($compte4); 
+afficherSolde($compte4)."<br>"; 
 
-
+echo "Débiter de 50 € sur tous les comptes <br>";
 // Débite 50 les comptes 1 a 4 et affiche le nouveau solde
 $compte1->debiter(50); 
-afficherSolde($compte1);
+afficherSolde($compte1)."<br>";
 
 $compte2->debiter(50); 
-afficherSolde($compte2);
+afficherSolde($compte2)."<br>";
+
 
 $compte3->debiter(50); 
-afficherSolde($compte3);
+afficherSolde($compte3)."<br>";
+
 
 $compte4->debiter(50); 
-afficherSolde($compte4);
+afficherSolde($compte4)."<br>";
 
-
-afficherSolde($compte1); // Affiche le solde du compte courant
-afficherSolde($compte2); // Affiche le solde du livret jeune
-afficherSolde($compte3); // Affiche le solde du livret PEL
-afficherSolde($compte4); // Affiche le solde du livret bloqué
 
 
 ?>
